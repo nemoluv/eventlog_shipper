788a12e85c54815b3242958b57ab9106cd87fa08
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-02-13 00:12:24 +0100

    update request logs

--- sql_queries/helper_notified_count.sql
+++ sql_queries/helper_notified_count.sql
@@ -1,4 +1,9 @@
-﻿SELECT requests.id, count(*)
+﻿
+update request_logs
+set 
+from(
+SELECT requests.id, count(*)
 FROM requests 
 JOIN helper_notifieds ON (helper_notifieds.request_id = requests.id)
-GROUP BY requests.id
+GROUP BY requests.id)
+WHERE request_log.request_id=subquery.address_id;
