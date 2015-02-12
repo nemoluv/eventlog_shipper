82d564de122829e49fa2984dde2e0b0b961c8ef5
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-02-12 23:30:34 +0100

    sql

---
+++ sql_queries/aggregate.sql
@@ -0,0 +1,10 @@
+﻿select requests.id, request_createds.created_at,  request_stoppeds.id as stopped, request_answereds.id as answered, request_cancelleds.id as cancelled
+from requests
+left outer join request_stoppeds 
+on request_stoppeds.request_id = requests.id
+left outer join request_answereds 
+on request_answereds.request_id = requests.id
+left outer join request_cancelleds 
+on request_cancelleds.request_id = requests.id
+left outer join request_createds 
+on request_createds.request_id = requests.id
