b8d656beec153d44f1506e87f295b596a6b655c7
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-02-12 23:37:42 +0100

    include timestampt

--- sql_queries/aggregate.sql
+++ sql_queries/aggregate.sql
@@ -1,7 +1,10 @@
 ﻿select requests.id, request_createds.created_at,  
 CASE WHEN request_stoppeds.id is not null then 1 else 0 end as stopped, 
 CASE WHEN request_answereds.id is not null then 1 else 0 end as answered,
-CASE WHEN request_cancelleds.id is not null then 1 else 0 end as cancelled
+CASE WHEN request_cancelleds.id is not null then 1 else 0 end as cancelled,
+request_stoppeds.created_at as stopped_at,
+request_answereds.created_at as answered_at,
+request_cancelleds.created_at as cancelled_at
 from requests
 left outer join request_stoppeds 
 on request_stoppeds.request_id = requests.id
@@ -10,4 +13,4 @@ on request_answereds.request_id = requests.id
 left outer join request_cancelleds 
 on request_cancelleds.request_id = requests.id
 left outer join request_createds 
-on request_createds.request_id = requests.id
+on request_createds.request_id = requests.id;
