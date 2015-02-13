bf1af72f60c223da5009a6e9a9037a648bf14322
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-02-13 08:29:37 +0100

    insert

--- sql_queries/aggregate.sql
+++ sql_queries/aggregate.sql
@@ -1,4 +1,5 @@
-﻿select requests.id, request_createds.created_at,  
+﻿insert into request_logs (reques_id, created_at, stopped, answered, cancelled, stopped_at, answered_at, cancelled_at, wait_length, call_length)(
+select distinct requests.id, request_createds.created_at,  
 CASE WHEN request_stoppeds.id is not null then 'yes' else 'no' end as stopped, 
 CASE WHEN request_answereds.id is not null then 'yes' else 'no' end as answered,
 CASE WHEN request_cancelleds.id is not null then 'yes' else 'no' end as cancelled,
@@ -15,4 +16,4 @@ on request_answereds.request_id = requests.id
 left outer join request_cancelleds 
 on request_cancelleds.request_id = requests.id
 left outer join request_createds 
-on request_createds.request_id = requests.id;
+on request_createds.request_id = requests.id);
